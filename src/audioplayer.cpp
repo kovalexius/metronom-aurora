96ac4042a792d523ac4d22f055f25ac72e56a916
Kristoffer Grönlund <krig@koru.se>
2014-01-06 17:29:39 +0100

    Code to acquire/release sound permissions. Doesn't work yet.

--- src/audioplayer.cpp
+++ src/audioplayer.cpp
@@ -4,6 +4,21 @@
 #include <QSoundEffect>
 
 #include <sailfishapp.h>
+#include <audioresource.h>
+
+namespace {
+    bool allow_sound = false;
+    audioresource_t *resource = NULL;
+
+    void on_acquired(audioresource_t *audio_resource, bool acquired, void *user_data)
+    {
+        if (acquired) {
+            allow_sound = true;
+        } else {
+            allow_sound = false;
+        }
+    }
+}
 
 AudioPlayer::AudioPlayer(QObject *parent) :
     QObject(parent)
@@ -14,9 +29,21 @@ AudioPlayer::AudioPlayer(QObject *parent) :
         "data/high.wav",
         "data/low.wav"
     };
+
+    // Use game because we're low latency. This classification is ...eh.
+    resource = audioresource_init(AUDIO_RESOURCE_GAME, on_acquired, user_data);
+
     for (size_t i = 0; i < sizeof(files) / sizeof(const char*); ++i) {
         sounds[i].setSource(SailfishApp::pathTo(files[i]));
     }
+
+    audioresource_acquire(resource);
+}
+
+AudioPlayer::~AudioPlayer() {
+    audioresource_release(resource);
+    audioresource_free(resource);
+    resource = NULL;
 }
 
 void AudioPlayer::playClick(int bar, int measure, int soundId) {
